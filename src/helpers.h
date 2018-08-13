5cf98c1bca308440fd6c3a57599d3e6ff433c9d9
Terry Lu <terrylu87@gmail.com>
2018-08-13 10:31:33 +0800

    seperate helper functions

---
+++ src/helpers.h
@@ -0,0 +1,18 @@
+#include <vector>
+
+// For converting back and forth between radians and degrees.
+constexpr double pi();
+double deg2rad(double x);
+double rad2deg(double x);
+
+double distance(double x1, double y1, double x2, double y2);
+
+int ClosestWaypoint(double x, double y, const std::vector<double> &maps_x, const std::vector<double> &maps_y);
+
+int NextWaypoint(double x, double y, double theta, const std::vector<double> &maps_x, const std::vector<double> &maps_y);
+
+// Transform from Cartesian x,y coordinates to Frenet s,d coordinates
+std::vector<double> getFrenet(double x, double y, double theta, const std::vector<double> &maps_x, const std::vector<double> &maps_y);
+
+// Transform from Frenet s,d coordinates to Cartesian x,y
+std::vector<double> getXY(double s, double d, const std::vector<double> &maps_s, const std::vector<double> &maps_x, const std::vector<double> &maps_y);
